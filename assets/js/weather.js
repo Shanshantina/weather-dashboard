209bb40dc23f5554341ff07aab1f27aa6b49cf74
Shanshan Xu <tinaxu84@gmail.com>
2021-01-11 20:11:54 -0500

    check the api keys linked to js and response the information from server

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,13 +1,27 @@
 var apiKey = "c6b02be382cc331562fa36791b952fda"
+var cityName = document.getElementById("cityname");
+var searchName = document.getElementById("searchName");
 
 
-var getWeatherInfo = function(city) {
+var getWeatherInfo = function(event) {
+
+    event.preventDefault();
+
+    var input = cityName.value.trim();
     
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=" + apiKey;
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + input + "&appid=" + apiKey;
 
     fetch(apiUrl).then(function(response) {
-        console.log(response);
+        return response.json(); 
+        
+    }) 
+    .then(function(data) {
+        console.log(data);
     })
+
+
 }
 
-getWeatherInfo();
+// getWeatherInfo();
+
+searchName.addEventListener("click", getWeatherInfo);
